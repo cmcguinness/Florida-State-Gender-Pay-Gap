215994137113bbc9bc510aca726ec31d323ccb88
Charles McGuinness <github@mcguinness.us>
2014-12-10 19:16:49 -0500

    Commenting code

--- titleShrink.R
+++ titleShrink.R
@@ -1,3 +1,15 @@
+# #############################################################################
+# titleShrink
+#
+# Find all the titles with "levels" in them or that have - GROUP appended
+# to them and remove that from the title.
+#
+# The intent is to flatting the levels within various job types, so a
+# HACKER I, HACKER II, and HACKER III - SES just get turned into HACKER
+#
+# This supports the logic that tries to see if there's a bias where women
+# are kept at lower grades than men
+# #############################################################################
 titleShrink <- function (salaries) {
   salaries$Class.Title <- gsub("- ...(\\s*)$","", salaries$Class.Title)
   salaries$Class.Title <- gsub("- ....(\\s*)$","", salaries$Class.Title)
