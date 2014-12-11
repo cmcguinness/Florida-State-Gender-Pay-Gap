7d9939e1326788c06e73a476288a8abd7debf8ea
Charles McGuinness <github@mcguinness.us>
2014-12-10 19:13:52 -0500

    Commented code

--- salaryOperations.R
+++ salaryOperations.R
@@ -1,4 +1,14 @@
-library(reshape)
+# #############################################################################
+# salaryByClass:
+#
+# Returns a list of lists. For each job classification, return a list of
+# salaries paid to males with that title, a list of salaries paid to females
+# with that title, and a list of salaries paid to people we cannot determine
+# but with that title.
+#
+# This gets used by subsequent analyses, and is basically just a restructuring
+# of the data to prepare it for downstream work.
+# #############################################################################
 
 salaryByClass <- function (in_salaries) {
   num <- 0
@@ -8,22 +18,31 @@ salaryByClass <- function (in_salaries) {
   females <- list()
   others <- list()
   
+  # We want to get the list of unique job titles, and the unique() function
+  # will do that for us...
   uniqueTitles <- unique(in_salaries$Class.Title)
   
-#cat("A total of ", length(uniqueTitles), " titles\n")
+  # cat("A total of ", length(uniqueTitles), " titles\n")
+  
+  # For each different title in the payroll data...
   for (title in uniqueTitles) {
+    # Not sure why, but some titles are blank, so skip them...
     if (title != "") {
 
       num <- num + 1
-#       if ((num %% 100) == 0) {
-#         cat("Loop #",num,"\n")
-#       }
+
+      # Add the title to the list ...
       titles[num] <- title
+      
+      # Create three vectors to hold the array of salaries
       temp_males <- numeric()
       temp_females <- numeric()
       temp_other <- numeric()
       
+      # Find all employees which have this title
       for (emp in which(in_salaries$Class.Title == title)) {
+        
+        # And add their salary data to the appropriate vector
         if (in_salaries$gender[emp] == "M") {
           temp_males <- c(temp_males, in_salaries$nSalary[emp])
         } else if (in_salaries$gender[emp] == "F") {
@@ -32,40 +51,70 @@ salaryByClass <- function (in_salaries) {
           temp_other <- c(temp_other, in_salaries$nSalary[emp])
         }
       }
+      
+      # Save the salary vectors into our lists
       males[[num]] <- temp_males
       females[[num]] <- temp_females
       others[[num]] <- temp_other
     }
   }
   
+  # Build the final list of lists...
   salByClass <- list(titles,males,females,others)
   names(salByClass) <- c("titles","males", "females", "others")
   return(salByClass)
 }
 
+# #############################################################################
+# computeBias
+#
+# Using the data as preprocessed by salaryByClass, compute the "normalized"
+# salary for each man and woman so long as the job class has at least one
+# man and one woman in it.
+#
+# The normalization logic consists of dividing the persons salary by the
+# average salary in the class.  Thus, a score of 1 means the person is
+# earning exactly the average paid to all workers.  A score of .9 means
+# the person is earning 90 cents on the dollar, so to speak.  A score of
+# 1.1 means the person is earning $1.10 when the average person is earning
+# $1.00.
+#
+# This is useful because we typically express bias in terms of how much
+# an average woman makes when an average man makes $1.  So, in this approach
+# we normalize everyone's salaries as if the average pay in each class is $1
+# #############################################################################
+
 computeBias <- function (salaryByClass) {
-#  if (file.exists("biasData.RData")) {
-#    load("biasData.RData", .GlobalEnv)
-#    return(biasData)
-#  }
   
+  # These will hold the list of normalized salaries for men and women
   mens <- numeric()
   womens <- numeric()
   
+  # Look at each title ....
   for (i in seq_along(salaryByClass$titles)) {
+    # If there is at least one man and one woman with that title
     if ((length(salaryByClass$males[[i]]) > 0 )  & (length(salaryByClass$females[[i]]) > 0)) {
+      # compute the average salary of all workers
       average <- mean(c(salaryByClass$males[[i]], salaryByClass$females[[i]], salaryByClass$others[[i]]))
+      # for all the women ...
       for (salary in salaryByClass$females[[i]]) {
+        # add their "normalized" salary to the female list
         womens <- c(womens, salary/average)
       }
+      # for all the men
       for (salary in salaryByClass$males[[i]]) {
+        # add their normalized salary to the male list
         mens <- c(mens, salary/average)
       }
+      
+      # while we use the unknowns' salaries to compute the average, we do not do anything
+      # with their salary specifically ...
     }
   }
   
+  # Make the list ... (remember, mens and womens are of unequal length, so this cannot be a data frame)
   biasData <- list(mens,womens)
   names(biasData) <- c("Mens", "Womens")
-#  save(biasData,file="biasData.RData")
+  # and give it back!
   return(biasData)
 }
